5cf4585071fdd715ed3e967656746160b1a85dcd
BlackNight414 <cristianariel375@gmail.com>
2024-12-02 00:06:08 -0300

    fix: arreglado importaciones de orquestador manual eliminado en el anterior commit

--- app/resources/comercio.py
+++ app/resources/comercio.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from app.services import OrquestadorManual, OrquestadorSaga
+from app.services import OrquestadorSaga
 import logging
 from app.services import MsCatalogo, MsInventario
 from app.mapping import CarritoSchema, ProductoSchema
--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,5 +2,4 @@ from .ms_catalogo import MsCatalogo
 from .ms_compras import MsCompras
 from .ms_pagos import MsPagos
 from .ms_inventario import MsInventario
-from .orquestador_manual import OrquestadorManual
 from .orquestador_saga import OrquestadorSaga
